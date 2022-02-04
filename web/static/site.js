e85197bef46ee51819a9f6b541ba7a8759635346
Stephen Haywood <stephen@averagesecurityguy.info>
2022-02-04 09:15:02 -0500

    Delete site.js
    Remove unnecessary file.

--- web/static/site.js
+++
@@ -1,7 +0,0 @@
-function search() {
-	term = document.getElementById("search").value;
-	bucket = document.getElementById("search").getAttribute("name");
-
-	url = "/search/" + bucket + "/" + term;
-	open(url, "_self");
-}
